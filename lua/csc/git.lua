490c08363fffdb14b1c423698536c58152ca19b0
teodord25 <djuric.teodor25@gmail.com>
2025-09-16 19:00:28 +0200

    feat(util): add git utils

---
+++ lua/csc/git.lua
@@ -0,0 +1,160 @@
+local M = {}
+
+function M.run_git_command(args, opts, callback)
+	opts = opts or {}
+
+	local cmd = { 'git' }
+	vim.list_extend(cmd, args)
+
+	local stdout_data = {}
+	local stderr_data = {}
+
+	local job_id = vim.fn.jobstart(cmd, {
+		cwd = opts.cwd or vim.fn.getcwd(),
+		stdout_buffered = true,
+		stderr_buffered = true,
+		on_stdout = function(_, data)
+			for _, line in ipairs(data) do
+				if line ~= '' then
+					table.insert(stdout_data, line)
+				end
+			end
+		end,
+		on_stderr = function(_, data)
+			vim.list_extend(stderr_data, data)
+		end,
+		on_exit = function(_, exit_code)
+			if exit_code == 0 then
+				callback(nil, stdout_data)
+			else
+				local error_msg = table.concat(stderr_data, '\n')
+				callback(error_msg ~= '' and error_msg or 'Git command failed', nil)
+			end
+		end,
+	})
+
+	if job_id == 0 then
+		callback('Failed to start git command', nil)
+	elseif job_id == -1 then
+		callback('Git command not executable', nil)
+	end
+
+	return job_id
+end
+
+function M.get_git_root(callback)
+	M.run_git_command(
+		{ 'rev-parse', '--show-toplevel' },
+		{},
+		function(err, output)
+			if err then
+				callback(err, nil)
+			else
+				local root = output[1] and vim.trim(output[1]) or nil
+				callback(nil, root)
+			end
+		end
+	)
+end
+
+function M.is_in_git_repo(callback)
+	M.run_git_command(
+		{ 'rev-parse', '--is-inside-work-tree' },
+		{},
+		function(err, output)
+			if err then
+				callback(nil, false)
+			else
+				local is_inside = output[1] and vim.trim(output[1]) == 'true'
+				callback(nil, is_inside)
+			end
+		end
+	)
+end
+
+function M.get_git_log(opts, callback)
+	opts = vim.tbl_extend('force', {
+		max_count = 100,
+		format = '%H|%s|%an|%ad',
+		date_format = 'short',
+	}, opts or {})
+
+	local args = {
+		'log',
+		'--pretty=format:' .. opts.format,
+		'--date=' .. opts.date_format,
+		'-n', tostring(opts.max_count)
+	}
+
+	M.run_git_command(
+		args,
+		{},
+		function(err, output)
+			if err then
+				callback(err, nil)
+				return
+			end
+
+			local commits = {}
+			for _, line in ipairs(output) do
+				if line and line ~= '' then
+					local parts = vim.split(line, '|')
+					if #parts >= 4 then
+						table.insert(commits, {
+							hash = parts[1],
+							subject = parts[2],
+							author = parts[3],
+							date = parts[4],
+						})
+					end
+				end
+			end
+
+			callback(nil, commits)
+		end
+	)
+end
+
+function M.test_git_commands(callback)
+	local results = {
+		git_available = false,
+		in_repo = false,
+		git_root = nil,
+		recent_commits = {},
+	}
+
+	M.run_git_command(
+		{ '--version' },
+		{},
+		function(err, output)
+			results.git_available = not err
+
+			if err then
+				callback(results)
+				return
+			end
+
+			M.is_in_git_repo(function(err, in_repo)
+				results.in_repo = in_repo
+
+				if not in_repo then
+					callback(results)
+					return
+				end
+
+				M.get_git_root(function(err, root)
+					results.git_root = root
+
+					M.get_git_log({ max_count = 5 }, function(err, commits)
+						if not err then
+							results.recent_commits = commits
+						end
+						callback(results)
+					end)
+				end)
+			end)
+		end
+	)
+end
+
+return M
