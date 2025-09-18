b43b353891acfb3b63a832bc39ad9b6f61ad678a
teodord25 <djuric.teodor25@gmail.com>
2025-09-18 21:16:10 +0200

    refactor(entrypoint): cleanup

--- plugin/csc.lua
+++ plugin/csc.lua
@@ -4,13 +4,7 @@ if vim.g.loaded_commit_scope then
 end
 vim.g.loaded_commit_scope = 1
 
--- version check ?
 if vim.fn.has('nvim-0.8.0') ~= 1 then
-	vim.api.nvim_err_writeln('commit-scope.nvim requires Neovim 0.8.0+')
+	vim.api.nvim_err_writeln('csc.nvim requires Neovim 0.8.0+')
 	return
 end
-
--- basic keymap to test plugin loading
-vim.keymap.set('n', '<leader>ct', '<cmd>CommitScopeTest<cr>', {
-	desc = 'Test commit scope plugin'
-})
